48a592b0e2c31e34c59b6623264e9870f0d2a152
ShinEunJin <sineun5501@naver.com>
2022-02-25 20:10:59 +0900

    template

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,27 +1,33 @@
-import Component from '../core/Component.js'
+import Component from "../core/Component.js";
 
 export default class Input extends Component {
+  constructor(template) {
+    this.template = template;
+  }
 
-    setup() {
-        this.$state = ""
-    }
+  setup() {
+    this.$state = "";
+  }
 
-    template() {
-        return `
-            <input class="inputText" type="text" value=${this.$state || ""} />
-            <button class="submitBtn">확인</button >
-        `
-    }
+  template() {
+    return this.template;
+  }
 
-    setEvent() {
-        this.$target.querySelector(".submitBtn").addEventListener('click', (e) => {
-            e.preventDefault()
-            console.log('state', this.$state)
-            fetch('http://localhost:5000/api/v1/content', { method: 'POST', body: JSON.stringify({ text: this.$state }), headers: { 'Content-Type': 'application/json' } }).then(res => console.log({ res })).catch(err => console.log({ err }))
-        })
+  setEvent() {
+    this.$target.querySelector(".submitBtn").addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("state", this.$state);
+      fetch("http://localhost:8080/api/v1/content", {
+        method: "POST",
+        body: JSON.stringify({ text: this.$state }),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => console.log({ res }))
+        .catch((err) => console.log({ err }));
+    });
 
-        this.$target.querySelector(".inputText").addEventListener('keyup', e => {
-            this.setState(e.target.value)
-        })
-    }
+    this.$target.querySelector(".inputText").addEventListener("keyup", (e) => {
+      this.setState(e.target.value);
+    });
+  }
 }
---
+++ src/templates/InputPresenter.js
@@ -0,0 +1,8 @@
+const InputPresenter = () => {
+  <>
+    <input class="inputText" type="text" value="test" />
+    <button class="submitBtn">확인</button>
+  </>;
+};
+
+export default InputPresenter;
